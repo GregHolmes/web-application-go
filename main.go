50e3446a47d91000726cfc53348a89a34069d3d7
Greg Holmes <greg.holmes2@vonage.com>
2020-09-06 21:50:05 +0100

    Added logout route and connecting the database on load

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ type LoginDetails struct {
 }
 
 func main() {
+	connectDb()
 
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/login", login)
 	mux.HandleFunc("/account", account)
 	mux.HandleFunc("/register", register)
+	mux.HandleFunc("/logout", logout)
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", mux)
