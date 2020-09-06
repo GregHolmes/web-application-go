5a2caf8bb1cb9a5a05db9ccdb1c34898f27ba5aa
Greg Holmes <greg.holmes2@vonage.com>
2020-09-06 21:49:04 +0100

    Updated auth.go to compare credentials with entries in the database, and sets a session on successful login

--- auth.go
+++ auth.go
@@ -4,9 +4,19 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "go-project-auth")
+
 	tmpl, err := template.ParseFiles("src/template/login.html")
 
 	if err != nil {
@@ -24,30 +34,51 @@ func login(w http.ResponseWriter, r *http.Request) {
 		Password: r.FormValue("password"),
 	}
 
-	// TODO: Compare user to any details in the database
-	_ = details
+	var user User
+
+	result := db.Where("email = ?", r.FormValue("email")).First(&user)
+
+	if result.RowsAffected != 1 {
+		// Throw error, no user found
+		fmt.Println("No user found")
+
+		return
+	}
+
+	hashPassResult := CheckPasswordHash(details.Password, user.Password)
+
+	if hashPassResult == false {
+		// Throw error, passwords don't match
+
+		fmt.Println("Passwords don't match")
+
+		return
+	}
+
+	// Set a session for this log in..
+	session.Values["authenticated"] = true
+	session.Save(r, w)
 
 	http.Redirect(w, r, "/account", http.StatusSeeOther)
 }
 
-// func setSession(userName string, response http.ResponseWriter) {
-// 	value := map[string]string{
-// 		"name": userName,
-// 	}
-// 	if encoded, err := cookieHandler.Encode("session", value); err == nil {
-// 		cookie := &http.Cookie{
-// 			Name:  "session",
-// 			Value: encoded,
-// 			Path:  "/",
-// 		}
-// 		http.SetCookie(response, cookie)
-// 	}
-// }
-
-func clearSession() {
-
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
-func logout() {
+func isAuthenticated(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "go-project-auth")
 
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Redirect(w, r, "/login", http.StatusForbidden)
+	}
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "go-project-auth")
+
+	// Revoke users authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
 }
